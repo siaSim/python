62ae974bbb0e2ab271982f18ba05f9c055e9f08e
시아 <102781433+2237043ssa@users.noreply.github.com>
2022-09-12 23:39:33 +0900

    class를 통한 부동산 매물 프로그램

---
+++ practice_class.py
@@ -0,0 +1,37 @@
+'''class Unit:
+    def __init__(self):
+        print("Unit 생성자")
+
+class Flyable:
+    def __init__(self):
+        print("Flyable 생성자")
+        
+class FlyableUnit(Unit, Flyable):
+    def __init__(self):
+        #super().__init__()
+        Unit.__init__(self)
+        Flyable.__init__(self)
+# 드랍쉽
+dropship = FlyableUnit()
+'''
+ Quiz) 주어진 코드를 활용하여 부동산 프로그램을 작성하시오.
+ 
+ (출력 예제)
+ 총 3대의 매물이 있습니다.
+ 강남 아파트 매매 10억 2010년
+ 마포 오피스텔 전세 5억 2007년
+ 송파 빌라 월세 500/50 2000년
+ 
+ [코드]
+ class House:
+      # 매물 초기화
+      def __init__(self, location, house_type, deal_type, price, completion_year):
+          self.location = location
+          self.house_type = house_type
+          self.price = price
+          
+      # 매물 정보 표시
+      def show_detail(self):
+          pass
+        #이어서 해야됌 9-최종
+        
